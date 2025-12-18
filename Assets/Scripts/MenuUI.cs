843e226f3761a1466fa31e375bd2c78ba485c210
Steel_Pact <enochenzeli@gmail.com>
2025-12-18 16:53:27 -0500

    Added basic setup with noodle bowl sprite

---
+++ Assets/Scripts/MenuUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour
+{
+    public string GameScene;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GameScene);
+    }
+}
